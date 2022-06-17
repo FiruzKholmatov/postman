300b4aa17e1e98581151dfb2e4725c85ae9e530a
firuz kholmatov <firuzkholmatov@yandex.ru>
2022-06-17 14:02:39 +0300

    test success

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -2,6 +2,7 @@ import io.restassured.specification.Argument;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class PostmanTest {
     @Test
@@ -10,14 +11,13 @@ public class PostmanTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
-// Выполняемые действия
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
                 .when()
                 .post("/post")
-// Проверки
                 .then()
                 .statusCode(200)
-                .body("https://postman-echo.com")
+                .body("[0].content-length", equalTo(null))
         ;
     }
 }
